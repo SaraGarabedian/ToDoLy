554d921fbe41316eb4db4b2d45cb8ff5706025bb
SaraGarabedian <sgarabediancenternode@gmail.com>
2019-10-13 11:55:35 +0200

    Modification of Menu Texts

--- src/Texts.java
+++ src/Texts.java
@@ -1,6 +1,6 @@
 public class Texts {
     public static final String MAIN_MENU = "\n>> Welcome to ToDoLy\n"+
-            ">> You have x tasks todo and Y tasks are done!\n" +
+            ">> You have %d tasks todo and %d tasks are done!\n" +
             ">> Pick an option: \n"+
             ">> (1) Add New Task\n" +
             ">> (2) Show Task List (by date or project)\n" +
